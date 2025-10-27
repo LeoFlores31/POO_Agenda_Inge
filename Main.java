c1be39b5ab6aca9a3a19f79a40012e0e5be62a3f
Leonel F <emilianoflores.g@hotmail.com>
2025-10-27 17:32:18 -0600

    Se crea el menu y los submenus correspondientes para paciente y agenda

--- Main.java
+++ Main.java
@@ -1,30 +1,12 @@
 import java.time.LocalDateTime;
 
+import utils.Menu;
+import utils.SubMenus;
+
 public class Main {
     public static void main(String[] args) {
-        // Crear paciente
-        Paciente paciente1 = new Paciente(1, "Leonel F", "3334564092", "leo@mail.com");
-        // Crear cita para ese paciente
-        Cita citaMatutina = new CitaMatutina(paciente1, LocalDateTime.now(), "Revision general");
-        if (citaMatutina.getMotivo() == null) {
-            System.out.println("Motivo incorrecto. Intenta de nuevo.");
-        } else {
-            citaMatutina.mostrarCita();
-        }
-
-
-        System.out.println("\n\n");
-
-        // Crear paciente
-        Paciente paciente2 = new Paciente(2, "Fer S", "1112223344", "fer@mail.com");
-        // Crear cita para ese paciente
-        Cita citaVespertina = new CitaVespertina(paciente2, LocalDateTime.now(), "Cita infantil");
-        if (citaVespertina.getMotivo() == null) {
-            System.out.println("Motivo " + citaVespertina.motivo +" incorrecto. Intenta de nuevo.");
-        } else {
-            citaVespertina.mostrarCita();
-        }
-
-
+        Menu menu = new Menu();
+        SubMenus.mostrarMenuAgenda();
+        SubMenus.mostrarMenuPaciente();
     }
 }
