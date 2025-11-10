bf3e25623aa2ed4da25eb110af38db38d08d714e
FerSoriano <fer8f10@hotmail.com>
2025-11-09 21:04:45 -0600

    se completa el metodo manejarModicarCita()

--- utils/ControladorCitas.java
+++ utils/ControladorCitas.java
@@ -1,9 +1,12 @@
 package utils;
 
 import model.Agenda;
-import model.cita.Cita;
+import model.Paciente;
+import model.cita.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -38,19 +41,104 @@ public class ControladorCitas {
                 continue;
             }
 
+            int opcion = -1;
+            do {
+                System.out.println("\nℹ️ Cita seleccionada:");
+                citaAModificar.mostrarCita();
 
-            // todo: crear otro bucle hasta que el usuario desee salir
-            System.out.println("\nℹ️ Cita seleccionada:");
-            citaAModificar.mostrarCita();
+                System.out.println("\nℹ️ Que quieres modificar?");
+                System.out.println("1) Fecha");
+                System.out.println("2) Hora");
+                System.out.println("3) Nombre del paciente");
+                System.out.println("4) Motivo");
+                System.out.println("5) Regresar al menu anterior");
+                System.out.print("Opcion: ");
+                try {
+                    opcion = sc.nextInt();
+                    sc.nextLine(); // limpiar el buffer
+                } catch (InputMismatchException e) {
+                    Menu.mostrarMensajeError("❌ Opcion Incorrecta. Intenta de nuevo.");
+                    sc.nextLine(); // limpiar el buffer
+                    continue;
+                }
 
-            System.out.println("\nℹ️ Que quieres modificar?");
-            System.out.println("1) Fecha");
-            System.out.println("2) Hora");
-            System.out.println("3) Nombre del paciente");
-            System.out.println("4) Motivo");
-            System.out.println("5) Regresar al menu anterior");
-            System.out.print("Opcion: ");
-            int opcion;
+                switch (opcion) {
+                    case 1:
+                        System.out.println("\nFecha actual: " + citaAModificar.getFecha());
+                        LocalDate nuevaFecha = preguntarFecha(sc);
+                        citaAModificar.setFecha(nuevaFecha);
+                        System.out.println("\n✅ Fecha modificada con exito!");
+                        break;
+                    case 2:
+                        String tipoCita = citaAModificar.getTipoCita();
+                        System.out.println("\nHora actual: " + citaAModificar.getHora());
+                        LocalTime nuevaHora = preguntarHora(sc);
+                        citaAModificar.setHora(nuevaHora);
+                        System.out.println("\n✅ Hora modificada con exito!");
+                        if (!tipoCita.equals(citaAModificar.getTipoCita())){
+                            Cita nuevaCita = cambiarInstanciaCita(citaAModificar, agenda);
+                            if (nuevaCita != null) {
+                                citaAModificar = agenda.getCitaPorId(nuevaCita.getId());
+                                System.out.println("\nℹ️ Tu tipo de cita cambio, favor de seleccionar un nuevo motivo.");
+                                // todo: crear metodos especificos para cada modificacion
+                                String nuevoMotivo = preguntarMotivo(sc, citaAModificar);
+                                citaAModificar.setMotivo(nuevoMotivo);
+                                System.out.println("\n✅ Motivo de cita modificado con exito!");
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println("\nNombre actual: " + citaAModificar.getPaciente().getNombre());
+                        String nuevoNombre = preguntarNombre(sc);
+                        citaAModificar.getPaciente().setNombre(nuevoNombre);
+                        System.out.println("\n✅ Nombre modificado con exito!");
+                        break;
+                    case 4:
+                        System.out.println("\nMotivo actual: " + citaAModificar.getMotivo());
+                        String nuevoMotivo = preguntarMotivo(sc, citaAModificar);
+                        citaAModificar.setMotivo(nuevoMotivo);
+                        System.out.println("\n✅ Motivo de cita modificado con exito!");
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Menu.mostrarMensajeError("❌ Opcion Incorrecta. Intenta de nuevo.");
+                }
+            } while (opcion != 5);
+
+        } while (true);
+    }
+
+    private static Cita cambiarInstanciaCita (Cita citaAModificar, Agenda agenda){
+        Paciente paciente = citaAModificar.getPaciente();
+        LocalDateTime nuevaFechaHora = citaAModificar.getFechaHora();
+        String motivo = citaAModificar.getMotivo();
+        int duracion = citaAModificar.getDuracionMinutos();
+
+        Cita nuevaCita = null;
+
+        if (citaAModificar.getTipoCita().equals("MATUTINA")) {
+            nuevaCita = new CitaMatutina(paciente, nuevaFechaHora, motivo, duracion);
+        } else if (citaAModificar.getTipoCita().equals("VESPERTINA")) {
+            nuevaCita = new CitaVespertina(paciente, nuevaFechaHora, motivo, duracion);
+        }
+
+        if (nuevaCita != null) {
+            if (!agenda.reemplazarCita(citaAModificar, nuevaCita)) {
+                Menu.mostrarMensajeError("❌ Error al cambiar la instnacia de la cita.");
+                return null;
+            }
+        }
+        return nuevaCita;
+    }
+
+    private static String preguntarMotivo(Scanner sc, Cita cita) {
+        System.out.println("\nLos motivos disponibles para tu horario son:");
+        cita.mostrarMotivosDisponibles();
+        int opcion = -1;
+        String nuevoMotivo = "";
+        do {
+            System.out.print("\tOpcion: ");
             try {
                 opcion = sc.nextInt();
                 sc.nextLine(); // limpiar el buffer
@@ -59,42 +147,52 @@ public class ControladorCitas {
                 sc.nextLine(); // limpiar el buffer
                 continue;
             }
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("\nFecha actual: " + citaAModificar.getFecha());
-                    LocalDate nuevaFecha = preguntarFecha(sc);
-                    citaAModificar.setFecha(nuevaFecha);
-                    System.out.println("\n✅ Fecha modificada con exito!");
-                    break;
-                // todo: implementar el resto de opciones
-                case 2:
-                    System.out.println("\nHora actual: " + citaAModificar.getHora());
-                    break;
-                case 3:
-                    System.out.println("\nNombre actual: " + citaAModificar.getPaciente().getNombre());
-                    break;
-                case 4:
-                    System.out.println("\nMotivo actual: " + citaAModificar.getMotivo());
-                    break;
-                case 5:
-                    break;
-                default:
-                    Menu.mostrarMensajeError("❌ Opcion Incorrecta. Intenta de nuevo.");
+            if (opcion <= 0 || opcion > cita.getTotalMotivos()){
+                Menu.mostrarMensajeError("❌ Opcion Incorrecta. Intenta de nuevo.");
+                continue;
             }
+            nuevoMotivo = cita.getMotivoPorIndice(opcion - 1);
+            System.out.println("\nOpcion seleccionada: " + nuevoMotivo);
+        } while (opcion <= 0 || opcion > cita.getTotalMotivos());
+        return nuevoMotivo;
+    }
 
-        } while (true);
+    private static String preguntarNombre(Scanner sc) {
+        String nuevoNombre;
+        do{
+            System.out.print("Nuevo nombre: ");
+            nuevoNombre = sc.nextLine().trim();
+            if (nuevoNombre.isEmpty()) {
+                Menu.mostrarMensajeError("❌ El nombre no puede estar vacío.");
+            } else if (nuevoNombre.length() == 1) {
+                Menu.mostrarMensajeError("❌ El nombre debe contener al menos 2 caracteres.");
+            }
+        } while (nuevoNombre.isEmpty() || nuevoNombre.length() == 1);
+        return nuevoNombre;
     }
 
     private static LocalDate preguntarFecha(Scanner sc) {
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         do {
             try {
-                System.out.print("Nueva Fecha (dd/MM/yyyy): ");
+                System.out.print("Nueva fecha (dd/MM/yyyy): ");
                 String fechaInput = sc.nextLine();
                 return LocalDate.parse(fechaInput, formatoFecha);
             } catch (Exception e) {
                 System.out.println("❌ Error: formato incorrecto. Ejemplo correcto -> Fecha: 05/10/2025");
+            }
+        } while (true);
+    }
+
+    private static LocalTime preguntarHora(Scanner sc) {
+        DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm");
+        do {
+            try {
+                System.out.print("Nueva hora (HH:mm): ");
+                String horaInput = sc.nextLine();
+                return LocalTime.parse(horaInput, formatoHora);
+            } catch (Exception e) {
+                System.out.println("❌ Error: formato incorrecto. Ejemplo correcto -> Hora: 14:30");
             }
         } while (true);
     }
