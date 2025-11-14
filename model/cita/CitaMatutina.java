60bb60a42c3eb8f0798d4fe3b9c4aad27fc9af50
FerSoriano <fer8f10@hotmail.com>
2025-11-14 15:45:14 -0600

    se cambian metodos para usar el nuevo HashMap

--- model/cita/CitaMatutina.java
+++ model/cita/CitaMatutina.java
@@ -7,14 +7,15 @@ import java.time.LocalDateTime;
 public class CitaMatutina extends Cita {
     public CitaMatutina(Paciente paciente, LocalDateTime fechaHora) {
         // constructor encadenado
-        this(paciente, fechaHora, "");
+        this(paciente, fechaHora, 1); // se asgina el motivo por default
     }
 
-    public CitaMatutina(Paciente paciente, LocalDateTime fechaHora, String motivo) {
-        super(paciente, fechaHora);
-        motivosDisponibles.put("Consulta nutricional", 60);
-        motivosDisponibles.put("Chequeo de glucosa", 40);
-        motivosDisponibles.put("Pesaje mensual", 15);
-        this.setMotivo(motivo);
+    public CitaMatutina(Paciente paciente, LocalDateTime fechaHora, int idMotivo) {
+        super(paciente, fechaHora, idMotivo);
+        motivosDisponibles.put(1, new MotivoCita("Consulta nutricional", 60));
+        motivosDisponibles.put(2, new MotivoCita("Chequeo de glucosa", 40));
+        motivosDisponibles.put(3, new MotivoCita("Pesaje mensual", 15));
+        this.setMotivo(motivosDisponibles.get(idMotivo).getMotivo());
+        this.setDuracionMinutos(motivosDisponibles.get(idMotivo).getDuracion());
     }
 }
