5816b3e29afaa8a15841919bef6a58edf930f2e3
LeoFlores31 <emilianoflores.g@hotmail.com>
2025-11-19 20:19:00 -0600

    Se arregla validación para cuando el usuario ingresa una letra lance mensaje de error y pida un número

--- Main.java
+++ Main.java
@@ -30,35 +30,42 @@ public class Main {
         int opcion;
 
         //////////// Bloque de prueba ////////////
-//        Paciente p1 = new Paciente("Fer", "33 1212 5555", "fer@email.com");
-//        Paciente p2 = new Paciente("Leo", "33 1508 2345", "leo@email.com");
-//
-//        gestorPacientes.agregarPaciente(p1);
-//        gestorPacientes.agregarPaciente(p2);
-//        gestorPacientesDAO.guardarPaciente(gestorPacientes.getListaPacientes());
+    //    Paciente p1 = new Paciente("Fer", "33 1212 5555", "fer@email.com");
+    //    Paciente p2 = new Paciente("Leo", "33 1508 2345", "leo@email.com");
+
+    //    gestorPacientes.agregarPaciente(p1);
+    //    gestorPacientes.agregarPaciente(p2);
+    //    gestorPacientesDAO.guardarPaciente(gestorPacientes.getListaPacientes());
         /////////////////////////////////////////
 
         Menu.mostrarMensaje("\tAgenda de Citas Medicas - El Inge 👨‍💻", 45);
 
-        do {
-            Menu.mostrarMenuPrincipal();
-            opcion = sc.nextInt();
-            // todo: validar entradad de datos
-            switch (opcion) {
-                case 1:
-                    SubMenus.ejecutarMenuPaciente(sc, gestorPacientes, gestorPacientesDAO);
-                    break;
-                case 2:
-                    SubMenus.ejecutarMenuAgenda(sc, agenda, agendaDAO, gestorPacientes);
-                    break;
-                case 3:
-                    System.out.println("Saliendo del sistema...");
-                    break;
-                default:
-                    System.out.println("Opción no válida, intenta de nuevo.");
-            }
+       do {
+        Menu.mostrarMenuPrincipal();
 
-        } while (opcion != 3);
+        System.out.print("Selecciona una opción: ");
+        while (!sc.hasNextInt()) {
+        System.out.println(" ❌ Entrada inválida. Por favor ingresa un número.");
+        System.out.print("Selecciona una opción: ");
+        sc.next(); // limpia la entrada incorrecta
+    }
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+        case 1:
+            SubMenus.ejecutarMenuPaciente(sc, gestorPacientes, gestorPacientesDAO);
+            break;
+        case 2:
+            SubMenus.ejecutarMenuAgenda(sc, agenda, agendaDAO, gestorPacientes);
+            break;
+        case 3:
+            System.out.println("Saliendo del sistema...");
+            break;
+        default:
+            System.out.println(" ❌ Opción no válida, intenta de nuevo.");
+        }
+
+    } while (opcion != 3);
 
         sc.close();
     }
